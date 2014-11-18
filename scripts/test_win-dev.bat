e9f7a9ba79ec803fbc59c0153baf6efb29890b12
Minoru Akagi <akaginch@gmail.com>
2014-11-18 11:43:20 +0900

    add test_win-dev.bat

---
+++ scripts/test_win-dev.bat
@@ -0,0 +1,26 @@
+@echo off
+rem test_win_dev.bat
+rem begin: 2014-11-18
+
+if not exist "output" mkdir output
+
+rem setlocal
+call C:\OSGeo4W64\bin\o4w_env.bat > nul
+echo  OSGEO4W_ROOT is %OSGEO4W_ROOT%
+
+rem -------------------------------------
+rem call "%OSGEO4W_ROOT%"\apps\grass\grass-6.4.3\etc\env.bat
+set PATH=%OSGEO4W_ROOT%\apps\qgis-dev\bin;%PATH%
+set QGIS_PREFIX_PATH=%OSGEO4W_ROOT:\=/%/apps/qgis-dev
+set GDAL_FILENAME_IS_UTF8=YES
+rem Set VSI cache to be used as buffer, see #6448
+rem set VSI_CACHE=TRUE
+rem set VSI_CACHE_SIZE=1000000
+rem set QT_PLUGIN_PATH=%OSGEO4W_ROOT%\apps\qgis-dev\qtplugins;%OSGEO4W_ROOT%\apps\qt4\plugins
+rem -------------------------------------
+set PYTHONPATH=%OSGEO4W_ROOT%\apps\qgis-dev\python
+
+python ..\run_test.py output\OSGeo4W64_dev.html
+rem endlocal
+
+pause
--- scripts/test_win-sa.bat
+++ scripts/test_win-sa.bat
@@ -1,5 +1,5 @@
 @echo off
-rem test_on_win.bat
+rem test_win_sa.bat
 rem begin: 2014-11-14
 rem osgeo4w_roots.txt: list of osgeo4w root directories
 
